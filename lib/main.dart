c4071f985215cc6db9e95bcd7cc7cf130757947e
Sébastien Bellon <seb.bellon@protonmail.com>
2022-06-21 19:59:59 +0200

    ✨ add images as asset

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,10 @@ void main() {
         backgroundColor: Colors.blueGrey[900],
       ),
       backgroundColor: Colors.blueGrey,
-      body: const Image(
-        image: NetworkImage(
-            "https://images.unsplash.com/photo-1504858700536-882c978a3464?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"),
+      body: const Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
       ),
     )),
   );
