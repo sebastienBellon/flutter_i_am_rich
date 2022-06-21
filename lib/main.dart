7cd067b0d1fc0aad9376711564c4433f3b0936ab
Sébastien bellon <sebastien.bellon@nagra.com>
2022-06-21 16:57:21 +0200

    ✨ add image for the background body of the app

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("I Am Rich"),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        backgroundColor: Colors.blueGrey,
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text("I Am Rich"),
+        backgroundColor: Colors.blueGrey[900],
       ),
-    ),
+      backgroundColor: Colors.blueGrey,
+      body: const Image(
+        image: NetworkImage(
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"),
+      ),
+    )),
   );
 }
