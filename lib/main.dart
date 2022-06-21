9ab2cbd3a8a2eb70eb1aa295adde70f16ddc754a
Sébastien Bellon <seb.bellon@protonmail.com>
2022-06-21 19:39:26 +0200

    ♻️ update image using unsplash

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
       backgroundColor: Colors.blueGrey,
       body: const Image(
         image: NetworkImage(
-            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"),
+            "https://images.unsplash.com/photo-1504858700536-882c978a3464?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"),
       ),
     )),
   );
